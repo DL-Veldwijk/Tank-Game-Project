718b6ebea016391dbaf945a29ea60a0e5368db87
Damiano Veldwijk <60340240+DL-Veldwijk@users.noreply.github.com>
2020-04-22 14:15:13 +0200

    Spawning Sys

---
+++ Tank Game Project/Assets/Scripts/PowerUps.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUps : MonoBehaviour
+{
+    public GameObject item;
+    public bool stopSpawning = false;
+    public float spawnTime;
+    public float spawnDelay;
+
+    public void Start()
+    {
+        Instantiate(item, transform.position, transform.rotation);
+    }
+
+    public void SpawnObject()
+    {
+        
+        if (stopSpawning)
+        {
+            CancelInvoke("SpawnObject");
+        }
+    }
+}
