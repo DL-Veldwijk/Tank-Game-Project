66a543ed6f0f6da4bedae6b080b1326831909ce1
Petrickvu <57941956+Petrickvu@users.noreply.github.com>
2020-04-27 14:01:32 +0200

    Input Controller Aangepast

--- Tank Game Project/Assets/Scripts/Movement.cs
+++ Tank Game Project/Assets/Scripts/Movement.cs
@@ -5,28 +5,6 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    PlayerControls controls;
 
-    Vector3 move;
-
-    private void Awake()
-    {
-        controls = new PlayerControls();        
-    }
-
-    void Update()
-    {
-        
-    }
-
-    void OnEnable()
-    {
-        controls.InputControler.Enable();
-    }
-
-    void OnDisable()
-    {
-        controls.InputControler.Disable();
-    }
 }
 
