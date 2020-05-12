9da4b8d52f46f4e42668a0abf8633bd56ec77ad2
Petrickvu <57941956+Petrickvu@users.noreply.github.com>
2020-05-12 13:50:23 +0200

    Movement

--- Tank Game Project/Assets/Scripts/Movement.cs
+++ Tank Game Project/Assets/Scripts/Movement.cs
@@ -5,6 +5,27 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    public Rigidbody rb;
 
+    private Vector3 bodyMovement;
+    public float speed;
+
+    public void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public void Update()
+    {
+        DoMove();
+    }
+
+    public void DoMove()
+    {
+        bodyMovement.x = Input.GetAxis("LeftCirclePadHor");
+        bodyMovement.z = Input.GetAxis("LeftCirclePadVer");
+        transform.Translate(bodyMovement * speed * Time.deltaTime);
+
+    }
 }
 
